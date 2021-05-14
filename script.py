9651f9e95c4429341c9994906b3b78b1ed2249b1
death0hole <niharbansal02@gmail.com>
2021-05-15 01:15:27 +0530

    Refactored for CSV Export

--- script.py
+++ script.py
@@ -2,10 +2,10 @@ import os
 import json
 import datetime
 
-fh = open('json.json')
-json_data = json.loads(fh.read())
+# service_curl_command = "curl -o json.json -X GET  -H \"User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36\"  \"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=457001&date="
+
 oh = open('data.txt', 'w')
-oh_aux = open('aux.json', 'w')
+# oh_aux = open('aux.json', 'w')
 
 date_1 = datetime.datetime.now().strftime("%d-%m-%Y")
 date_2 = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%d-%m-%Y")
@@ -17,9 +17,24 @@ while True:
     cont = input("Continue?")
     if(not cont):break
 
+
     for test_date in test_dates:
-        curl_command = "curl -o json.json -X GET  -H \"User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36\"  \"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=457001&date=" + test_date + "\""
+        print(test_date)
+        curl_command = "curl -o jsonHolder.json -X GET  -H \"User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36\"  \"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=457001&date=" + test_date + "\""
         os.system(curl_command)
+
+        fhAux = open('jsonHolder.json')
+        jsonAux_data = json.loads(fhAux.read())
+        
+        try:
+            aux_var = (jsonAux_data['centers'][0])
+        except:
+            continue
+
+        os.system("cp jsonHolder.json json.json")
+
+        fh = open('json.json')
+        json_data = json.loads(fh.read())
 
         for center in json_data['centers']:
             name = center['name']
@@ -32,9 +47,9 @@ while True:
             vaccine = center['sessions'][0]['vaccine']
 
 
-            # if(availability == 0):      # Just for testing
-            #     os.system('notify-send "Slot Available!!" -i "/home/death0hole/Desktop/CoWin/syringe.jpg" -u critical')
-            #     os.system("aplay /home/death0hole/Desktop/CoWin/alert.wav -q")
+            if(availability == 0):      # Just for testing
+                os.system('notify-send "Slot Available!!" -i "/home/death0hole/Desktop/CoWin/syringe.jpg" -u critical')
+                os.system("aplay /home/death0hole/Desktop/CoWin/alert.wav -q")
 
 
             oh.write("Center Name")
