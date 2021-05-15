35bd3ecdf49f61acead5efba717e7e844a0e80b0
death0hole <niharbansal02@gmail.com>
2021-05-15 18:50:21 +0530

    Used urllib

--- script.py
+++ script.py
@@ -1,7 +1,10 @@
+import urllib.request, urllib.parse, urllib.request
 import os, time
 import json
 import datetime
 import cfg
+
+service_url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?"
 
 date_1 = datetime.datetime.now().strftime("%d-%m-%Y")
 date_2 = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%d-%m-%Y")
@@ -9,31 +12,39 @@ date_3 = (datetime.datetime.now() + datetime.timedelta(days=2)).strftime("%d-%m-
 
 test_dates = [date_1, date_2, date_3]
 
+def request_data(date):
+    params = {
+        'pincode': cfg.constants['pin'],
+        'date': date
+    }
+
+    hdr = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+        'accept': 'application/json',
+        'Accept-Language': 'en_US'
+    }
+
+    URL = service_url + urllib.parse.urlencode(params)
+    req = urllib.request.Request(URL, headers=hdr)
+    jsonResp = urllib.request.urlopen(req).read()
+    return jsonResp.decode()
+
 def work():
     while True:
         # cont = input("Continue?")
         # if(not cont):break
         # time.sleep(5)
-        time.sleep(60)
 
 
         for test_date in test_dates:
-            print(test_date)
-            curl_command = "curl -o jsonHolder.json -X GET  -H \"User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36\"  \"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=" + str(cfg.constants['pin']) + "&date=" + test_date + "\""
-            os.system(curl_command)
+            # print(test_date)
+            jsonStr = request_data(test_date)
+            json_data = json.loads(jsonStr)
 
-            fhAux = open('jsonHolder.json')
-            jsonAux_data = json.loads(fhAux.read())
-            
             try:
-                aux_var = (jsonAux_data['centers'][0])
+                aux_var = (json_data['centers'][0])
             except:
                 continue
-
-            os.system("cp jsonHolder.json json.json")
-
-            fh = open('json.json')
-            json_data = json.loads(fh.read())
 
             oh = open('data.txt', 'w')
 
@@ -80,6 +91,7 @@ def work():
 
             oh.close()
 
+        time.sleep(60)
 
 def notify(center_name):
     sysStr = 'notify-send' + ' "' + center_name + ' || Slot Available!!" -i "/home/death0hole/Desktop/CoWin/syringe.jpg" -u critical'
@@ -91,3 +103,4 @@ def notify(center_name):
 
 if __name__ == '__main__':
     work()
+    # print(request_data('15-05-2021'))
