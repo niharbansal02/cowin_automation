fb98d21380a0c93ab2902c34d4e8b703924a1fc1
death0hole <niharbansal02@gmail.com>
2021-05-16 01:52:22 +0530

    Modular Approach; Used classes

--- script.py
+++ script.py
@@ -12,88 +12,74 @@ date_3 = (datetime.datetime.now() + datetime.timedelta(days=2)).strftime("%d-%m-
 test_dates = [date_1, date_2, date_3]
 
 
-def request_data(date):
-    service_url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?"
-    params = {
-        'pincode': cfg.constants['pin'],
-        'date': date
-    }
+class CoWin_alerts:
+    def request_data(self, date):
+        service_url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?"
+        params = {
+            'pincode': cfg.constants['pin'],
+            'date': date
+        }
 
-    hdr = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
-        'accept': 'application/json',
-        'Accept-Language': 'en_US'
-    }
+        hdr = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+            'accept': 'application/json',
+            'Accept-Language': 'en_US'
+        }
 
-    URL = service_url + urllib.parse.urlencode(params)
-    req = urllib.request.Request(URL, headers=hdr)
-    jsonResp = urllib.request.urlopen(req).read()
-    return jsonResp.decode()
+        URL = service_url + urllib.parse.urlencode(params)
+        req = urllib.request.Request(URL, headers=hdr)
+        jsonResp = urllib.request.urlopen(req).read()
+        return jsonResp.decode()
 
-def work():
-    while True:
-        # cont = input("Continue?")
-        # if(not cont):break
-        # time.sleep(5)
+    def work(self):
+        while True:
+            # cont = input("Continue?")
+            # if(not cont):break
+            # time.sleep(5)
+
+            for test_date in test_dates:
+                print(test_date)
+                jsonStr = self.request_data(test_date)
+                json_data = json.loads(jsonStr)
+
+                try:
+                    aux_var = (json_data['centers'][0])
+                except:
+                    continue
+
+                oh = open('data.txt', 'w')
+
+                for center in json_data['centers']:
+                    name = center['name']
+                    city = center['district_name']
+                    pin = center['pincode']
+                    fee_type = center['fee_type']
+                    min_age = center['sessions'][0]['min_age_limit']
+                    date = center['sessions'][0]['date']
+                    availability = center['sessions'][0]['available_capacity']
+                    vaccine = center['sessions'][0]['vaccine']
 
 
-        for test_date in test_dates:
-            print(test_date)
-            jsonStr = request_data(test_date)
-            json_data = json.loads(jsonStr)
+                    # if(availability == 0):      # Just for testing
+                    if(availability > 0):
+                        notifObj = Notifier()
+                        notifObj.notify("!! Slot Available !!", name)
 
-            try:
-                aux_var = (json_data['centers'][0])
-            except:
-                continue
+                    self.write_in_file(oh, "Center Name", name)
+                    self.write_in_file(oh, "Availability", availability)
+                    self.write_in_file(oh, "Date", date)
+                    self.write_in_file(oh, "Vaccine", vaccine)
+                    self.write_in_file(oh, "Min Age", min_age)
 
-            oh = open('data.txt', 'w')
+                oh.close()
 
-            for center in json_data['centers']:
-                name = center['name']
-                city = center['district_name']
-                pin = center['pincode']
-                fee_type = center['fee_type']
-                min_age = center['sessions'][0]['min_age_limit']
-                date = center['sessions'][0]['date']
-                availability = center['sessions'][0]['available_capacity']
-                vaccine = center['sessions'][0]['vaccine']
+            time.sleep(60)
 
-
-                # if(availability == 0):      # Just for testing
-                if(availability > 0):
-                    notifObj = Notifier()
-                    notifObj.notify("!! Slot Available !!", name)
-
-                oh.write("Center Name")
-                oh.write("\t")
-                oh.write(name)
-                oh.write("\n")
-
-                oh.write("Availability")
-                oh.write("\t")
-                oh.write(str(availability))
-                oh.write("\n")
-                
-                oh.write("Date")
-                oh.write("\t")
-                oh.write(date)
-                oh.write("\n")
-                
-                oh.write("Vaccine")
-                oh.write("\t")
-                oh.write(vaccine)
-                oh.write("\n")
-                
-                oh.write("Min Age")
-                oh.write("\t")
-                oh.write(str(min_age))
-                oh.write("\n")
-                oh.write("\n")
-
-            oh.close()
-
-        time.sleep(60)
+    def write_in_file(self, fout, head, msg):
+        fout.write(head)
+        fout.write("\t")
+        fout.write(msg)
+        fout.write("\n")
 
 
 
